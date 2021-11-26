3f7cd10a62b64259f10a991b594c07d7612e2c77
Artur Gasparyan <gasparyanartur99@gmail.com>
2021-11-26 14:50:59 +0100

    Fix player type

--- main.c
+++ main.c
@@ -7,14 +7,19 @@ int main() {
     printf("Starting program.\n");
 
     brd_t board = {.tiles={0}, .remainingTiles=BOARD_SIZE};
-    print_board(&board);
+    brd_t *board_ptr = &board;
 
-    set_tile(&board, 2, 1, CROSS);
-    print_board(&board);
+    print_board(board_ptr);
 
-    set_tile(&board, 2, 3, CROSS);
-    set_tile(&board, -1, 1, CROSS);
-    print_board(&board);
+    set_tile(board_ptr, 2, 1, CROSS);
+    print_board(board_ptr);
+
+    set_tile(board_ptr, 0, 0, NAUGHT);
+    print_board(board_ptr);
+
+    set_tile(board_ptr, 2, 3, CROSS);
+    set_tile(board_ptr, -1, 1, CROSS);
+    print_board(board_ptr);
 
     return 0;
 }
