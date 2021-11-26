0c21eef672b509137a52d33938b805ac22f0aae9
Artur Gasparyan <gasparyanartur99@gmail.com>
2021-11-26 15:25:55 +0100

    Update board

--- main.c
+++ main.c
@@ -6,19 +6,20 @@
 int main() {
     printf("Starting program.\n");
 
-    brd_t board = {.tiles={0}, .remainingTiles=BOARD_SIZE};
-    brd_t *board_ptr = &board;
+    board_t board = {.tiles={0}, .remainingTiles=BOARD_SIZE};
+    board_t *board_ptr = &board;
 
     print_board(board_ptr);
 
     set_tile(board_ptr, 2, 1, CROSS);
     print_board(board_ptr);
 
-    set_tile(board_ptr, 0, 0, NAUGHT);
+    set_tile(board_ptr, 1, 2, NAUGHT);
     print_board(board_ptr);
 
     set_tile(board_ptr, 2, 3, CROSS);
     set_tile(board_ptr, -1, 1, CROSS);
+
     print_board(board_ptr);
 
     return 0;
--- src/board.c
+++ src/board.c
@@ -3,23 +3,27 @@
 #include "stdbool.h"
 
 uint_fast8_t coord_to_index(uint_fast8_t x, uint_fast8_t y);
-tile_t *get_tile_ptr(brd_t *board, uint_fast8_t x, uint_fast8_t y);
-bool coords_in_board(uint_fast8_t x, uint_fast8_t y);
+bool coord_in_board(uint_fast8_t x, uint_fast8_t y);
+tile_ptr get_tile_ptr(board_ptr board, uint_fast8_t x, uint_fast8_t y);
 
-void print_board(brd_t *board) {
+/*
+ * Public Methods
+ */
+
+void print_board(board_ptr board) {
     printf("===========\n|  %d %d %d  |\n|  %d %d %d  |\n|  %d %d %d  |\n",
            board->tiles[0], board->tiles[1], board->tiles[2],
            board->tiles[3], board->tiles[4], board->tiles[5],
            board->tiles[6], board->tiles[7], board->tiles[8]);
 }
 
-void set_tile(brd_t *board, uint_fast8_t x, uint_fast8_t y, player_t player) {
+void set_tile(board_ptr board, uint_fast8_t x, uint_fast8_t y, player_t player) {
     if (!is_player_t(player)) {
         printf("Attempted to interpret value '%d' as a player type.\n", player);
         return;
     }
 
-    if (!coords_in_board(x, y)) {
+    if (!coord_in_board(x, y)) {
         printf("Attempted to reference illegal board coordinate (%d,%d).\n", x, y);
         return;
     }
@@ -27,15 +31,27 @@ void set_tile(brd_t *board, uint_fast8_t x, uint_fast8_t y, player_t player) {
     *get_tile_ptr(board, x, y) = player;
 }
 
-tile_t *get_tile_ptr(brd_t *board, uint_fast8_t x, uint_fast8_t y) {
-    int index = coord_to_index(x, y);
-    return &board->tiles[index];
+player_t get_tile(board_ptr board, uint_fast8_t x, uint_fast8_t y) {
+    if (!coord_in_board(x, y)) {
+        printf("Attempted to reference illegal board coordinate (%d,%d).\n", x, y);
+        return NO_PLAYER;
+    }
+
+    return *get_tile_ptr(board, x, y);
 }
+
+/*
+ * Private Methods
+ */
 
 uint_fast8_t coord_to_index(uint_fast8_t x, uint_fast8_t y) {
     return y * BOARD_WIDTH + x;
 }
 
-bool coords_in_board(uint_fast8_t x, uint_fast8_t y) {
+tile_ptr get_tile_ptr(board_ptr board, uint_fast8_t x, uint_fast8_t y) {
+    return &board->tiles[coord_to_index(x, y)];
+}
+
+bool coord_in_board(uint_fast8_t x, uint_fast8_t y) {
     return x < BOARD_WIDTH && y < BOARD_HEIGHT;
 }
