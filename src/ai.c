5d3391c23f19aaea20225508f9cab62d43efd6de
Artur Gasparyan <gasparyanartur99@gmail.com>
2021-11-28 13:15:50 +0100

    Add to minimax

--- src/ai.c
+++ src/ai.c
@@ -52,77 +52,68 @@ tile_t calculate_optimal_move(board_ptr board, player_t aiPlayer) {
     init_move_score(move_scores, board->remainingTiles);
 
     // TODO Implement remaining moves using linked list to remove unnecessary looping over taken values
+    // TODO Fix bug
 
     int depth = 0;
     bool maximizing = true;
     player_t enemyPlayer = get_other_player(aiPlayer);
+    tile_t optimalTile = -1;
 
     tile_t tile = 0;
     while (tile < BOARD_SIZE) {
         if (!is_tile_empty(board, tile)) {
             tile++;
-            continue;
-        }
-
-        if (maximizing) {
-            player_t winner = get_winner(board);
-            if (winner == aiPlayer) {
-                move_scores[depth] = MAX_SCORE;
-                maximizing = false;
-                tile = move_stack[depth--] + 1;
-            }
-            else {  // Guaranteed no winner since opponent can't win on AI's turn.
-                if (is_board_full(board)) {
-                    if (move_scores[depth] < 0)
-                        move_scores[depth] = 0;
-                    maximizing = false;
-                    tile = move_stack[depth--] + 1;
-                }
-                else {  // Make move on empty tile
-                    move_stack[depth++] = tile;
-                    take_tile(board, tile, aiPlayer);
-                    maximizing = false;
-                    tile = 0;
-                }
-            }
         }
         else {
-            player_t winner = get_winner(board);
-            if (winner == enemyPlayer) {
-                move_scores[depth] = MIN_SCORE;
-                maximizing = true;
-                tile = move_stack[depth--] + 1;
-            }
-            else {  // Guaranteed no winner since AI can't win on opponent's turn
-                if (is_board_full(board)) {
-                    if (move_scores[depth] > 0)
-                        move_scores[depth] = 0;
+            if (maximizing) {
+                player_t winner = get_winner(board);
+                if (winner == aiPlayer) {
+                    move_scores[depth] = MAX_SCORE;
+                    optimalTile = tile;
+                    maximizing = false;
+                    tile = move_stack[depth--] + 1;
+                } else {  // Guaranteed no winner since opponent can't win on AI's turn.
+                    if (is_board_full(board)) {
+                        if (move_scores[depth] < 0) {
+                            move_scores[depth] = 0;
+                            optimalTile = tile;
+                        }
+                        maximizing = false;
+                        tile = move_stack[depth--] + 1;
+                    } else {  // Make move on empty tile
+                        move_stack[depth++] = tile;
+                        take_tile(board, tile, aiPlayer);
+                        maximizing = false;
+                        tile = 0;
+                    }
+                }
+            } else {
+                player_t winner = get_winner(board);
+                if (winner == enemyPlayer) {
+                    move_scores[depth] = MIN_SCORE;
                     maximizing = true;
                     tile = move_stack[depth--] + 1;
-                }
-                else {  // Make move on empty tile
-                    move_stack[depth++] = tile;
-                    take_tile(board, tile, enemyPlayer);
-                    maximizing = true;
-                    tile = 0;
+                } else {  // Guaranteed no winner since AI can't win on opponent's turn
+                    if (is_board_full(board)) {
+                        if (move_scores[depth] > 0)
+                            move_scores[depth] = 0;
+                        maximizing = true;
+                        tile = move_stack[depth--] + 1;
+                    } else {  // Make move on empty tile
+                        move_stack[depth++] = tile;
+                        take_tile(board, tile, enemyPlayer);
+                        maximizing = true;
+                        tile = 0;
+                    }
                 }
             }
+        }
+
+        if (tile == BOARD_SIZE && depth > 0) {
+            tile = move_stack[depth--] + 1;
+            maximizing = !maximizing;
         }
     }
 
-
-    return 0;
-}
-
-void makeMove(board_ptr board, tile_t *move_stack, int *depth, player_t *currentPlayer, tile_t *tile) {
-    move_stack[(*depth)++] = (*tile);
-    take_tile(board, (*tile), (*currentPlayer));
-    (*currentPlayer) = get_other_player((*currentPlayer));
-    (*tile) = 0;
-}
-
-void undoMove(board_ptr board, const tile_t *move_stack, int *depth, player_t *currentPlayer, tile_t *tile) {
-    (*tile) = move_stack[(*depth)--];
-    clear_tile(board, (*tile)++);
-    (*currentPlayer) = get_other_player((*currentPlayer));
+    return optimalTile;
 }
